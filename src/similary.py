f26d7c4300b6590e9b8100183c282e1a07febb2e
Carlos Manuel González Peña <cmglezpdev@gmail.com>
2024-04-19 23:00:02 -0400

    feature(nlp): functions to calc embed of a list of texts

--- src/similary.py
+++ src/similary.py
@@ -13,24 +13,25 @@ def save_topics_embeddings():
     # I assume the cosistency of the data
     with open('./data/topics.txt', 'r') as f:
         topics = f.read().split('\n')
-        embeddings = []
-        
-        for i in range(0, len(topics), VOYAGE_EMBED_BATCH_SIZE):
-            batch = topics[i:i+VOYAGE_EMBED_BATCH_SIZE]
-            print(len(batch))
-            result = vo.embed(batch, model='voyage-lite-02-instruct')
-            print(result.total_tokens)
-            
-            for j in range(len(batch)):
-                embeddings.append({
-                    'topic': topics[i + j],
-                    'embedding': result.embeddings[j] 
-                })
-                    
+        embeddings = embed(topics)            
         pickle.dump(embeddings, open('./data/topic-embed.pkl', 'wb'))    
         with open('./data/topic-embed.json', 'w') as f:
             f.write(json.dumps(embeddings))
+
+def embed(texts: list[str]):
+    vo = voyageai.Client(api_key=VOYAGE_API_KEY)
+    embeddings = []
+
+    for i in range(0, len(texts), VOYAGE_EMBED_BATCH_SIZE):
+        batch = texts[i:i+VOYAGE_EMBED_BATCH_SIZE]
+        result = vo.embed(batch, model='voyage-lite-02-instruct')
         
-        
+        for j in range(len(batch)):
+            embeddings.append({
+                'topic': texts[i + j],
+                'embedding': result.embeddings[j] 
+            })
+    return embeddings
+
 # save_topics_embeddings()
 # load_topics_embeddings()
