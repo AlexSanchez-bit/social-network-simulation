cf9fc602251a730e0cb8ff761a263c07ce03777b
Carlos Manuel González Peña <cmglezpdev@gmail.com>
2024-04-19 23:02:00 -0400

    chore(): add new constants

--- src/constants.py
+++ src/constants.py
@@ -3,5 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+VOYAGE_EMBED_BATCH_SIZE = 128
 OPENAI_BASE_URL = os.environ.get('OPENAI_BASE_URL')
 OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
+CLAUDE_API_KEY = os.environ.get('CLAUDE_API_KEY')
+VOYAGE_API_KEY = os.environ.get('VOYAGE_API_KEY')
