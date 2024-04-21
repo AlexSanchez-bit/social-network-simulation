cbaa5f01b45e3bb5d6dc4e0d3f4fae30dd6d4add
Carlos Manuel González Peña <cmglezpdev@gmail.com>
2024-04-20 20:17:57 -0400

    feature(): add function to build correlation matrix

--- src/nlp/similary.py
+++ src/nlp/similary.py
@@ -5,19 +5,32 @@ import numpy as np
 from ..constants import VOYAGE_API_KEY, VOYAGE_EMBED_BATCH_SIZE
 
 def load_topics_embeddings():
-    embeddings = pickle.load(open('./data/topic-embed.pkl', 'rb'))
+    embeddings = pickle.load(open('./data/topics-embeddings.pkl', 'rb'))
     return embeddings
 
 def save_topics_embeddings():
-    vo = voyageai.Client(api_key=VOYAGE_API_KEY)
-
     # I assume the cosistency of the data
     with open('./data/topics.txt', 'r') as f:
         topics = f.read().split('\n')
         embeddings = embed(topics)            
-        pickle.dump(embeddings, open('./data/topic-embed.pkl', 'wb'))    
-        with open('./data/topic-embed.json', 'w') as f:
-            f.write(json.dumps(embeddings))
+        pickle.dump(embeddings, open('./data/topics-embeddings.pkl', 'wb'))
+
+def load_topics_correlation_matrix():
+    mat_embedd = pickle.load(open('./data/topics-corr-matrix.pkl', 'rb'))
+    return mat_embedd
+
+def save_topics_correlation_matrix():
+    topics_embedd = load_topics_embeddings()
+    embeddings = np.array([x['embedding'] for x in topics_embedd])
+    sim = len(embeddings) * [[]]
+    for i, t in enumerate(embeddings):
+        for j, tt in enumerate(embeddings):
+            sim[i].append({
+                'id': j,
+                'sim': cosine_distance(t, tt)
+            })
+    pickle.dump(sim, open('./data/topics-corr-matrix.pkl', 'wb'))    
+
 
 def embed(texts: list[str]):
     vo = voyageai.Client(api_key=VOYAGE_API_KEY)
